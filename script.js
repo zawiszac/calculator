acd90d5ef0ace51d40d9b2cb9c77cc7ae0dac8ed
Christian Zawisza <zawiszac@msu.edu>
2020-07-21 11:35:35 -0400

    Operate function

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+function operate(operator, x, y) {
+
+    let result;
+    switch (operator) {
+
+        case '+':
+            result = add(x.y);
+            break;
+
+        case '-':
+            result = subtract(x.y);
+            break;
+
+        case '*':
+            result = multiply(x.y);
+            break;
+
+        case '/':
+            result = divide(x.y);
+            break;
+    }
+
+    return result;
+}
