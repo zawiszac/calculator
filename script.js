9ba9b8f2113d01ff3a303be90ed7c37d68171edd
Christian Zawisza <zawiszac@msu.edu>
2020-07-31 14:28:42 -0400

    Created functions for each button that perform the calculations and populate the display

--- script.js
+++ script.js
@@ -14,60 +14,111 @@ function divide(x, y) {
     return x / y;
 }
 
-function operate(operator, args) {
+function operate(operator, x, y) {
 
-    let result;
+    let result = 0;
     switch (operator) {
 
         case '+':
-            result = add(x.y);
+            result = add(x, y);
             break;
 
         case '-':
-            result = subtract(x.y);
+            result = subtract(x, y);
             break;
 
         case '*':
-            result = multiply(x.y);
+            result = multiply(x, y);
             break;
 
         case '/':
-            result = divide(x.y);
-            break;
-
-        case 'DEL':
-            break;
-        
-        case 'C':
-            break;
-        
-        case '%':
-            break;
-
-        case '+/-':
-            break;
-
-        case '=':
+            result = divide(x, y);
             break;
     }
-
     return result;
 }
 
 function runCalculator() {
 
-    let display = document.getElementById('display');
-    let buttons = document.querySelectorAll('.calc-button');
-    let displayContent = "";
-    if (display.textContent !== undefined) displayContent = display.textContent;
+    let storageDisplay = document.getElementById('storage-display');
+    let currentDisplay = document.getElementById('current-display');
+    let calcButtons = document.querySelectorAll('.calc-button');
+    let opButtons = document.querySelectorAll('.op-button');
+    let leftOperandPresent = false;
+    let operatorPresent = false;
+    let resultPresent = false;
 
-    buttons.forEach((button) => {
+    calcButtons.forEach((button) => {
         button.addEventListener('click', (e) => {
-            let input = e.textContent;
-            displayContent += input;
-            e.textContent = displayContent;
-            console.log(displayContent);
-        });          
+            if (!resultPresent) {
+                let input = e.target.textContent;
+                currentDisplay.textContent += input;
+                leftOperandPresent = true;
+            }
+        });
+    });
+
+    opButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            if (leftOperandPresent && !operatorPresent) {
+                resultPresent = false;
+                operatorPresent = true;
+                let input = e.target.textContent;
+                currentDisplay.textContent += input;
+                storageDisplay.textContent = currentDisplay.textContent;
+                currentDisplay.textContent = "";
+                leftOperandPresent = false;
+            }
+        });
+    });
+
+    let equalsButton = document.getElementById('equals');
+    equalsButton.addEventListener('click', () => {
+        if (leftOperandPresent && operatorPresent) {
+            let operator = storageDisplay.textContent.slice(storageDisplay.textContent.length - 1);
+            let leftOperand = Number(storageDisplay.textContent.slice(0, storageDisplay.textContent.length - 1));
+            let rightOperand = Number(currentDisplay.textContent);
+            let result = operate(operator, leftOperand, rightOperand);
+            storageDisplay.textContent += currentDisplay.textContent;
+            currentDisplay.textContent = result;
+            leftOperandPresent = true;
+            operatorPresent = false;
+            resultPresent = true;
+        }
+    });
+
+    let clearButton = document.getElementById('clear');
+    clearButton.addEventListener('click', () => {
+        currentDisplay.textContent = "";
+        storageDisplay.textContent = "";
+        leftOperandPresent = false;
+        operatorPresent = false;
+        resultPresent = false;
+    })
+
+    let deleteButton = document.getElementById('delete');
+    deleteButton.addEventListener('click', () => {
+        if (!resultPresent) {
+            currentDisplay.textContent = currentDisplay.textContent.slice(0, currentDisplay.textContent.length - 1);
+        }
+    });
+
+    let percentButton = document.getElementById('percentage');
+    percentButton.addEventListener('click', () => {
+        if (leftOperandPresent && !operatorPresent) {
+            currentDisplay.textContent = (Number(currentDisplay.textContent) * 0.01);
+        }
+    });
+
+    let signButton = document.getElementById('sign');
+    signButton.addEventListener('click', () => {
+        console.log(typeof(currentDisplay.textContent[0]));
+        if (!resultPresent && leftOperandPresent && currentDisplay.textContent[0] !== '-') {
+            currentDisplay.textContent = '-' + currentDisplay.textContent;
+        }
+        else if (!resultPresent && leftOperandPresent && currentDisplay.textContent[0] === '-'){
+            currentDisplay.textContent = currentDisplay.textContent.slice(1);
+        }
     });
 }
 
